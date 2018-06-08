3387fc1b5ed0b0bcca963aaaef0577814be671c5
moshesc <beepbeep9860@gmail.com>
2018-06-08 09:06:32 +0100

    StringLength
    The Length property in String Class returns the total number of characters in a String

---
+++ StringLength/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace StringLength
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1 = "Welcome to Csharpstar !! ";
+            Console.WriteLine(str1.Length);
+        }
+    }
+}
