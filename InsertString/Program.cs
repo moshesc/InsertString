b356539b332db2528a5a7735edb014c654c05537
moshesc <beepbeep9860@gmail.com>
2018-06-08 08:49:13 +0100

    Add project files.

---
+++ InsertString/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InsertString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
