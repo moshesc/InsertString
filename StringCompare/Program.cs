5703693f56582988e6a8ec1b85d29cf6598c77ac
moshesc <beepbeep9860@gmail.com>
2018-06-08 09:41:39 +0100

    The CSharp String Compare function compares two strings lexicographically . The comparison is based on the Unicode value of each character in the string.

---
+++ StringCompare/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace StringCompare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1 = "Csharpstar ";
+            string str2 = "CSHARPSTAR";
+            Console.WriteLine(string.Compare(str1, str2));
+        }
+    }
+}
